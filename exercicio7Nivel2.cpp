ab9d924e4d60a6b3675dcfba9f79f40e9a30231b
Vpx2104 <162385592+Vpx2104@users.noreply.github.com>
2024-04-19 00:35:48 +0000

    m

---
+++ exercicio7Nivel2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    string frutas [5];
+
+    frutas [0] = "acerola";
+    frutas [1] = "amora";
+    frutas [2] = "damasco";
+    frutas [3] = "kiwi";
+    frutas [4] = "abacaxi";
+
+    cout << "a fruta na posiçao 3 é: " << frutas[3] << endl;
+
+    return 0;
+}
---
+++ exercicio8Nivel2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    string cores [7];
+
+    cores [0] = "azul";
+    cores [1] = "preto";
+    cores [2] = "branco";
+    cores [3] = "cinza";
+    cores [4] = "castanho";
+    cores [5] = "verde";
+    cores [6] = "ciano";
+
+    cout << "as cores disponiveis são: se a cor desejada for azul digite 0, se for preto digite 1, branco digite 2, cinza digite 3, castanho digite 4, verde digite 5, ciano digite 6. Digite o numero da cor desejada: " << endl;
+    cin >> co
+
+    return 0;
+}
